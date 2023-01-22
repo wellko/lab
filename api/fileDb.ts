3024e649f798928492bee03835356462e4909b1c
Kwon Valerii <wellko24@gmail.com>
2023-01-22 15:31:05 +0600

    changed getItems() for getting last 30 messages

--- api/fileDb.ts
+++ api/fileDb.ts
@@ -1,5 +1,6 @@
 import { promises as fs } from 'fs';
 import {Message, SendData} from "./types";
+import {randomUUID} from "crypto";
 
 const filename = './db.json';
 
@@ -14,18 +15,26 @@ const fileDb = {
             data = [];
         }
     },
+
     async getItems() {
+        if (data.length > 30){
+            return data.slice(-30);
+        }
         return data;
     },
 
+    async getItemsAfterDate(date: string) {
+        return data.filter(element => element.datetime > date)
+    },
+
     async addItem(item: SendData) {
-        const id = crypto.randomUUID();
-
-
-        const product = {id, ...item};
-        data.push(product);
+        const id = randomUUID();
+        const date = new Date();
+        const datetime = date.toISOString();
+        const message = {id, datetime, ...item};
+        data.push(message);
         await this.save();
-        return product;
+        return message;
     },
 
     async save() {
